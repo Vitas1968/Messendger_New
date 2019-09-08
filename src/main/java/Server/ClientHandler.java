f1188e60550650ee1e3f38e78b2f8927e3ba375c
Vitas1968 <dor1968@mail.ru>
2019-09-08 19:46:45 +0700

    Initial commit

---
+++ src/main/java/Server/ClientHandler.java
@@ -0,0 +1,237 @@
+package Server;
+
+import java.io.*;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ClientHandler implements Runnable
+{
+    private Socket socket;
+    private DataOutputStream out;
+    private DataInputStream in;
+    private Server server;
+    private String nick;
+    // путь к папке хранилищу сообщений клиента
+    private static final String path =
+            "D:\\GeekBrains\\Education_Projects\\Messenger1\\src\\Storage";
+    //файл хранилище для клиента
+    private File storage;
+
+
+
+    public ClientHandler(Server server, Socket socket) {
+
+
+        try {
+            this.socket = socket;
+            this.server = server;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    //  new Thread(new Runnable() {
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                String str = in.readUTF();
+                if (str.startsWith("/auth")) {
+                    String[] tokens = str.split(" ");
+                    String newNick = AuthService.getNickByLoginAndPass(tokens[1], tokens[2]);
+                    if (newNick != null) {
+                        if (!server.isNickBusy(newNick)) {
+                            sendMsg("/authok");
+                            nick = newNick;
+                            server.subscribe(ClientHandler.this);
+                            break;
+                        } else {
+                            sendMsg("Учетная запись уже используется");
+                        }
+                    } else {
+                        sendMsg("Неверный логин/пароль!");
+                    }
+                }
+            }
+            // создание/чтегие файла хранилища
+            // если такого файла нет он создается
+            // если есть, читаем из него сторки
+            // и выводим в интерфейс клиента
+            storage=createStorage(path);
+
+            //  ArrayList<String> arrayList = readStorage(storage);
+//                        for (int i = 0; i<arrayList.size()-1 ; i++)
+//                        {
+//
+//                            System.out.println(arrayList.get(i));
+//                        }
+
+            outInGUI(readStorage(storage));
+
+            while (true) {
+                String str = in.readUTF();
+                System.out.println("Client " + str);
+
+                // реализация личного сообщения
+                if (str.startsWith("/w"))
+                {
+                    String[] tokens = str.split(" ",3);
+                    server.sendPersonalMsg(ClientHandler.this, tokens[1], tokens[2]);
+                }
+                if (str.equals("/end")) {
+                    out.writeUTF("/serverClosed");
+                    break;
+                }
+                server.broadcastMsg(nick + " : " + str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            server.unsubscribe(ClientHandler.this);
+            server.broadcastMsg(ClientHandler.this.getNick()+" "+"покинул чат");
+        }
+    }
+    // }).start();
+    // создание хранилища сообщений для клиента
+    private File createStorage(String path)
+    {
+        File newFile =new File(path,"Storage_"+nick+".txt");
+        if(newFile.exists()) return newFile;
+        else
+        {
+
+            boolean create = false;
+            try
+            {
+                create = newFile.createNewFile();
+
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+            if (create) return newFile;
+            else return null;
+        }
+    }
+
+    // чтение истории из хранилища
+    private ArrayList<String> readStorage(File storage)
+    {
+        ArrayList<String> list= new ArrayList();
+//        try (FileInputStream source = new FileInputStream(storage))
+//        {
+//            Scanner sc = new Scanner(source);
+//            while (sc.hasNext())
+//            {
+//                list.add(sc.nextLine());
+//                System.out.println(sc.nextLine());
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        String line;
+        try (DataInputStream dis = new DataInputStream(new FileInputStream(storage)))
+        {
+            while ((line=dis.readUTF())!=null)
+            {
+                list.add(line);
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return list;
+
+    }
+    // вывод истории в интерфейс клиента
+    private void outInGUI(ArrayList<String> storageList)
+    {
+
+
+        for (int iter=storageList.size()<=100 ? 0 : storageList.size()-100 ; iter <storageList.size()-1 ; iter++)
+        {
+            sendMsg(storageList.get(iter));
+        }
+    }
+
+    // запись сообщения в файл
+    public void saveMsgStorage(String msg)
+    {
+        if (storage!=null)
+        {
+//            try (FileWriter writer = new FileWriter(storage, true))
+//            {
+//                // запись всей строки
+//
+//                writer.write(msg + "\r\n");
+//                writer.flush();
+//            } catch (IOException ex)
+//            {
+//
+//                System.out.println(ex.getMessage());
+//            }
+            try(DataOutputStream dos = new DataOutputStream(new FileOutputStream(storage,true)))
+            {
+                dos.writeUTF(msg);
+            } catch (FileNotFoundException e)
+            {
+                e.printStackTrace();
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        } else throw new NullPointerException();
+
+    }
+
+    public String getNick()
+    {
+        return nick;
+    }
+
+
+    public void sendMsg(String msg) {
+
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ src/main/java/Server/Server.java
@@ -0,0 +1,105 @@
+package Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server
+{
+    private Vector<ClientHandler> clients;
+    // создание executorService
+    private ExecutorService executorService;
+
+    public Server() {
+        clients = new Vector<>();
+        ServerSocket server = null;
+        Socket socket = null;
+        executorService= Executors.newFixedThreadPool(clients.size());
+
+
+
+        try {
+            AuthService.connect();
+            server = new ServerSocket(8086);
+            System.out.println("Сервер запущен!");
+
+            while (true) {
+                socket = server.accept();
+                // создаем хендлер и добавляем в пул
+                executorService.execute( new ClientHandler(this, socket));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AuthService.disconnect();
+            executorService.shutdown();
+        }
+    }
+    //проверка есть ли такой клиент в чате
+    public boolean isNickBusy(String nick) {
+        for (ClientHandler o : clients) {
+            if (o.getNick().equals(nick)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    // рассылка сообщений всем участникам чата
+    public void broadcastMsg(String msg) {
+        for (ClientHandler o: clients) {
+            if (msg.startsWith(o.getNick())) {
+                // добавленный к строке пробел метка - свой
+                String tmp=" "+msg;
+                o.sendMsg(tmp);
+                o.saveMsgStorage(msg);
+                continue;
+            }
+            o.sendMsg(msg);
+            o.saveMsgStorage(msg);
+        }
+    }
+
+
+    // отправка личного сообщения указанному абоненту
+    public void sendPersonalMsg(ClientHandler from, String nickTo, String msg) {
+        for (ClientHandler o : clients) {
+            if (o.getNick().equals(nickTo)) {
+                o.sendMsg("from " + from.getNick() + ": " + msg);
+                from.sendMsg("to " + nickTo + ": " + msg);
+                return;
+            }
+        }
+        from.sendMsg("Клиент с ником " + nickTo + " не найден в чате");
+    }
+
+    // добавление клиента в чат
+    public void subscribe(ClientHandler client) {
+        clients.add(client);
+    }
+
+    //удаление клиента из чата
+    public void unsubscribe(ClientHandler client) {
+        clients.remove(client);
+    }
+}
